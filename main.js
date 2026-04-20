83f4aee40040dfa46d4cdb5e655a7a336b726d1f
Math <mf8483226@gmail.com>
2026-04-19 22:25:46 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+//FUNDO BINARIO
+  document.querySelectorAll('.background-binary').forEach( elemento => {
+
+    const binarios = Array.from({ length: 2500 }, () => Math.round(Math.random())).join('');
+    elemento.insertAdjacentHTML('afterbegin', `<div class="fundo-binario">${binarios}</div>`);
+  });
+
+//FUNDO GEMA
+  document.querySelectorAll('.background-gema').forEach( elemento => {
+
+    const gemas = Array.from({ length: 2500 }, () => "GEMA").join(' ');
+    elemento.insertAdjacentHTML('afterbegin', `<div class="fundo-gematico">${gemas}</div>`);
+  });
+
+
+    const carrossel = document.getElementById('gallery-carrousel');
+    const btnLeft = document.querySelector('.left-arrow');
+    const btnRight = document.querySelector('.right-arrow');
+
+    function moverDireita() {
+        let tamanhoItem = carrossel.firstElementChild.clientWidth + 30;
+        carrossel.scrollBy({ left: tamanhoItem, behavior: 'smooth' });
+    }
+
+    function moverEsquerda() {
+        let tamanhoItem = carrossel.firstElementChild.clientWidth + 30;
+        carrossel.scrollBy({ left: -tamanhoItem, behavior: 'smooth' });
+    }
+
+    btnRight.addEventListener('click', moverDireita);
+    btnLeft.addEventListener('click', moverEsquerda);
+
+    setInterval(() => {
+        let fimDoCarrossel = carrossel.scrollWidth - carrossel.clientWidth - 10;
+        
+        if (carrossel.scrollLeft >= fimDoCarrossel) {
+            carrossel.scrollTo({ left: 0, behavior: 'smooth' });
+        } else {
+            moverDireita();
+        }
+    }, 30000);
+
+
+
+});
+
+
+
+
+
+
+const modal = document.getElementById('modal-overlay');
+const btnClose = document.getElementById('close-modal');
+
+function awardCard(img, title, description) {
+    document.getElementById('modal-img').src = img;
+    document.getElementById('modal-title').innerText = title;
+    document.getElementById('modal-body').innerHTML = description;
+    
+    modal.classList.add('active'); //gambiarra que mudo de classe p abrir modal
+}
+
+
+if (btnClose && modal) {
+    btnClose.addEventListener('click', () => {
+
+        modal.classList.remove('active'); //gambiarra de mudar de classe para fechar o modal, pq se eu usar display none ele perde a animação
+
+    });
+}
+
